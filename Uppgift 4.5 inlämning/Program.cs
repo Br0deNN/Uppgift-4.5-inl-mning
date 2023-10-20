3fe9f337309c6584fb52735ed182df99edf54b1a
7R1XY <theo.broden@gmail.com>
2023-10-20 13:15:31 +0200

    Add project files.

---
+++ Uppgift 4.5 inlämning/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace uppgift_4_5
+{
+    class program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Skriv in ett heltal");
+            string heltal = Console.ReadLine();
+            string tal = "";
+            for (int i = 0; i < heltal.Length; i++)
+            {
+                string tecken = heltal[i].ToString();
+                int siffra = int.Parse(tecken);
+                siffra++;
+
+                if (siffra == 9+1)
+                {
+                    siffra = 0;
+                }
+
+                tal += siffra;
+
+            }
+            Console.WriteLine(tal);
+            Console.ReadKey();
+        }
+    }
+}
